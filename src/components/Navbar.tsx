2ccc4149ad76191a2aa27dcfb754f37c5fb96b8f
divinefoxx <divine.foxx0@gmail.com>
2026-09-12 20:56:52 -0400

    New post

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,7 +7,7 @@ export default function Navbar() {
                 <Link to="/">Home</Link>
                 <Link to="/info">Info</Link>
             </ul>
-            <p>ARC Weather Center v1.7.6</p>
+            <p>ARC Weather Center v1.7.7</p>
         </nav>
     );
 }
